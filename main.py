8b50da58452e77d7af3c21e97d06297fd1dea8ab
raczynskid <raczynski90@gmail.com>
2024-01-12 09:41:37 +0100

    main method initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import scraper
+import dbmanager
+
+results = scraper.get_feed()
+db_file = "D:\Python\GalaxusScraper\database\database"
+conn = dbmanager.create_connection(db_file)
+with conn:
+    for rec in results:
+        dbmanager.create_record(conn, rec.format_as_tuple())
+
+conn.close()
